f9e761f08296e71f536042f168b3d02ba169d401
DudkinVladislav <90377440+DudkinVladislav@users.noreply.github.com>
2021-10-18 22:02:25 +0300

    Update calcul.js

--- calcul.js
+++ calcul.js
@@ -22,8 +22,8 @@ window.addEventListener('DOMContentLoaded',function(event) {
     console.log("DOM finaly loaded and parsed");
     const chek =/^\d+$/
     let num1 = document.getElementsByName("number1");
-	if(chek.test(num1[0].value==false){
-	   document.getElementById('out').innerHTML='Введённые данные неверны.';}
+	if(chek.test(num1[0].value==false)
+	   {document.getElementById('out').innerHTML='Введённые данные неверны.';}
     let num3 = document.getElementById("out");
     let sel = document.getElementByName("myselect");
     sel[0].addEventListener("change",function(event){
